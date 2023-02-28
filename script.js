9bac74f87031928a98abdbe024b5d551e31e02fc
Devin McDaniel <dmcdaniel9@gmail.com>
2023-02-28 18:55:01 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const authors = ["Filip Bunkins", "Juli Kosolapova", "Mark Zimmer", "Sebastien Jermer", "Steve Carter"];
+
+const imageSources = ["beach-sj", "desert-jk", "mountains-sc", "river-mz", "snow-fb"];
+
+const leftButton = document.querySelector('#left-btn');
+const rightButton = document.querySelector('#right-btn');
+const attributionElem = document.querySelector('.attribution');
